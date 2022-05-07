cde23ba937dc18ddd666287d4d1b0ea5b33de590
bkcelebi <burakkcelebi@hotmail.com>
2022-05-07 20:40:32 +0100

    starting the user login / authentication system

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
-from flask import Flask, render_template, url_for
+import bcrypt
+from flask import Flask, render_template, url_for, redirect
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin
+from flask_wtf import FlaskForm
+from wtforms import PasswordField, SubmitField, EmailField, StringField, IntegerField
+from wtforms.validators import InputRequired, Length, ValidationError
+from flask_bcrypt import Bcrypt
 # from datetime import datetime
 
+#creating the app and database
+#creating bcrypt to hash the password
 app = Flask(__name__)
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 app.config['SECRET_KEY'] = 'secretkey'
 
@@ -18,25 +26,75 @@ app.config['SECRET_KEY'] = 'secretkey'
 #     def __repr__(self):
 #         return '<task %r>' % self.id
 
+#creating user table in the database
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(50), nullable=False, unique=True)
     username = db.Column(db.String(20), nullable=False)
     password = db.Column(db.String(80), nullable=False)
     age = db.Column(db.Integer, nullable=False)
     gender = db.Column(db.String(30), nullable=False)
     location = db.Column(db.String(50), nullable=False)
 
+#creating the signup form
+class SignUpFrom(FlaskForm):
+    email = EmailField(validators=[InputRequired(), Length(
+        min=5, max=50)], render_kw={"placeholder": "Email"})
+    password = PasswordField(validators=[InputRequired(), Length(
+        min=5, max=20)], render_kw={"placeholder":"Password"})
+    username = StringField(validators=[InputRequired(), Length(
+        min=5, max=20)], render_kw={"placeholder","username"})
+    age = IntegerField(validators=[InputRequired(), Length(
+        min=2, max=2)], render_kw={"placeholder", "age"})
+    gender = StringField(validators=[InputRequired(), Length(
+        min=4, max=30)], render_kw={"placeholder","gender"})
+    location = StringField(validators=[InputRequired(), Length(
+        min=4, max=50)], render_kw={"placeholder","location"})
+    
+    submit = SubmitField("Register")
+
+    #validating the email to make sure that one email has
+    #only one account
+    def validate_email(self, email):
+        existing_email = User.query.filter_by(
+            email=email.data).first()
+        if existing_email:
+            raise ValidationError("This email already exists.")
+
+#creating the login form
+class LoginFrom(FlaskForm):
+    email = EmailField(validators=[InputRequired(), Length(
+        min=5, max=50)], render_kw={"placeholder": "Email"})
+    password = PasswordField(validators=[InputRequired(), Length(
+        min=5, max=20)], render_kw={"placeholder":"Password"})
+    submit = SubmitField("Login")
+
+
+
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-    return render_template('login.html')
+    form = LoginFrom()
+    return render_template('login.html', form=form)
 
-@app.route('/signup')
+@app.route('/signup', methods=['GET', 'POST'])
 def signup():
-    return render_template('register.html')
+    form = SignUpFrom()
+
+    #have a look at location to upgrade it
+    if form.validate_on_submit():
+        hashed_pw = bcrypt.generate_password_hash(form.password.data)
+        new_user = User(email=form.email.data, password=hashed_pw,
+        username=form.username.data, age=form.age.data, 
+        gender=form.gender.data, location=form.location.data)
+        db.session.add(new_user)
+        db.session.commit()
+        return redirect('index.html', form=form)
+
+    return render_template('signup.html', form=form)
 
 if __name__ == "__main__":
     app.run(debug=True)

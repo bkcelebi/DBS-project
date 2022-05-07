385ed11f536babb7b97fe7b3c7e965844bd4f80d
bkcelebi <burakkcelebi@hotmail.com>
2022-05-07 21:47:53 +0100

    starting the user login / authentication system

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
+import email
 import bcrypt
-from flask import Flask, render_template, url_for, redirect
+from flask import Flask, render_template, url_for, redirect, flash
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import UserMixin
+from flask_login import UserMixin, login_user, LoginManager, login_required, logout_user, current_user
 from flask_wtf import FlaskForm
 from wtforms import PasswordField, SubmitField, EmailField, StringField, IntegerField
 from wtforms.validators import InputRequired, Length, ValidationError
@@ -15,6 +16,15 @@ db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 app.config['SECRET_KEY'] = 'secretkey'
+
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = "login"
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
 
 
@@ -44,8 +54,8 @@ class SignUpFrom(FlaskForm):
         min=5, max=20)], render_kw={"placeholder": "Password"})
     username = StringField(validators=[InputRequired(), Length(
         min=5, max=20)], render_kw={"placeholder": "username"})
-    age = IntegerField(validators=[InputRequired(), Length(
-        min=2, max=2)], render_kw={"placeholder": "age"})
+    age = IntegerField(validators=[InputRequired()],
+        render_kw={"placeholder": "age"})
     gender = StringField(validators=[InputRequired(), Length(
         min=4, max=30)], render_kw={"placeholder": "gender"})
     location = StringField(validators=[InputRequired(), Length(
@@ -60,6 +70,7 @@ class SignUpFrom(FlaskForm):
             email=email.data).first()
         if existing_email:
             raise ValidationError("This email already exists.")
+            #flash("This email already exists.")
 
 #creating the login form
 class LoginFrom(FlaskForm):
@@ -71,14 +82,10 @@ class LoginFrom(FlaskForm):
 
 
 
+
 @app.route('/')
 def index():
     return render_template('index.html')
-
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    form = LoginFrom()
-    return render_template('login.html', form=form)
 
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
@@ -92,9 +99,34 @@ def signup():
         gender=form.gender.data, location=form.location.data)
         db.session.add(new_user)
         db.session.commit()
-        return redirect('index.html', form=form)
+        return redirect(url_for('index'))
 
     return render_template('signup.html', form=form)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginFrom()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user:
+            if bcrypt.check_password_hash(user.password, form.password.data):
+                login_user(user)
+                return redirect(url_for('ads'))
+    return render_template('login.html', form=form)
+
+@app.route('/logout', methods=['GET', 'POST'])
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+
+
+@app.route('/ads', methods=['GET', 'POST'])
+@login_required
+def ads():
+    return render_template('ads.html')
+
 
 if __name__ == "__main__":
     app.run(debug=True)

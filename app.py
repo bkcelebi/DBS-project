a2ab82d67a173e795991ed729ba30a4adbb6f7a1
bkcelebi <burakkcelebi@hotmail.com>
2022-05-07 18:35:10 +0100

    starting the user login / authentication system

--- app.py
+++ app.py
@@ -1,22 +1,42 @@
 from flask import Flask, render_template, url_for
 from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime
+from flask_login import UserMixin
+# from datetime import datetime
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 db = SQLAlchemy(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+app.config['SECRET_KEY'] = 'secretkey'
 
-class Todo(db.Model):
+
+
+# class Todo(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     content = db.Column(db.String(200), nullable=False)
+#     date_created = db.Column(db.DateTime, default=datetime.utcnow)
+
+#     def __repr__(self):
+#         return '<task %r>' % self.id
+
+class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
-    content = db.Column(db.String(200), nullable=False)
-    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+    username = db.Column(db.String(20), nullable=False)
+    password = db.Column(db.String(80), nullable=False)
+    age = db.Column(db.Integer, nullable=False)
+    gender = db.Column(db.String(30), nullable=False)
+    location = db.Column(db.String(50), nullable=False)
 
-    def __repr__(self):
-        return '<task %r>' % self.id
-
-@app.route('/', methods=['POST', 'GET'])
+@app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/signup')
+def signup():
+    return render_template('register.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
